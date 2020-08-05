557b7bf18ffacda0edaba6d9ecea7467c450a5d5
Guilherme <guilherme1067@hotmail.com>
2020-08-05 11:49:26 -0300

    created Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import "./styles.css"
+//stateless component
+const Header = () => (
+    <header id="main-header">Welcome To ReactJS</header>
+);
+
+
+export default Header;
